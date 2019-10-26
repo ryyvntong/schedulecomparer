cb08eb16da8e801b7938dad8df5e63da20bd0d92
davidrmur <davidrmurga@gmail.com>
2019-10-25 20:37:37 -0400

    layout & jumbotron

---
+++ src/components/HeroBanner/HeroBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const heroBanner = ()=>{
+    return(
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+    )
+};
+
+export default heroBanner;
---
+++ src/containers/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroBanner from '../../components/HeroBanner/HeroBanner';
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <>
+            <p>Toolbar </p>
+            <HeroBanner />
+            <p>Features </p>
+            </>
+        );
+    }
+}
+
+export default LandingPage;
