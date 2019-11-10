3529c658aea5d50f35d7e3ce9dffebde2142dc5b
davidrmur <davidrmurga@gmail.com>
2019-11-10 11:16:23 -0500

    setting up redux, not in use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers,  } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+// const sagaMiddleware = createSagaMiddleware();
+
+// const rootReducer = combineReducers({
+// });
+
+// const store = createStore(rootReducer, composeEnhancers (
+//     applyMiddleware(sagaMiddleware)
+// ));
+
+// sagaMiddleware.run();
+
+// const app = (
+//     <Provider store={store} >
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+//     </Provider>
+// );
+
+// ReactDOM.render(app, document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
